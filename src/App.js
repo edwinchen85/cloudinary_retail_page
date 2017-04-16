2b9fc4da8fe375f22fe0f235e32c1c9ff3738626
edwinchen85 <edwinchen85@gmail.com>
2017-04-16 10:29:36 +0800

    Use Cloudinary to adjust RGB channels from original color with simple transformation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
+import { SketchPicker } = 'react-color';
 import './App.css';
 
-const ImageTransformations = ({width, selectedShirt}) => {
+const ImageTransformations = ({width, rgb, selectedShirt, text}) => {
   return (
     <Image publicId={selectedShirt.main+'.jpg'}>
       <Transformation width={width} crop="scale" />
+      <Transformation effect={'red:'+((-1+rgb.r/255)*100).toFixed(0)} />
+      <Transformation effect={'blue:'+((-1+rgb.b/255)*100).toFixed(0)} />
+      <Transformation effect={'green:'+((-1+rgb.g/255)*100).toFixed(0)} />
+      <Transformation underlay={selectedShirt.underlay} flags="relative" width="1.0" />
+      <Transformation overlay={selectedShirt.overlay} flags="relative" width="1.0" />
     </Image>
   );
 };
@@ -20,8 +26,18 @@ class App extends Component {
         {id: 2, main: 'laying-shirt'},
         {id: 3, main: 'hanging_t-shirt'}
       ],
-      selectedShirt: defaultShirt
+      selectedShirt: defaultShirt,
+      background: {rgb: {
+        r:255,
+        g:255,
+        b:255}
+      }
     };
+  }
+
+  handleColorChange(color) {
+    // Updates color
+    this.setState({ background: color }, _ => this.forceUpdate());
   }
 
   selectedShirt(thumb) {
@@ -29,9 +45,19 @@ class App extends Component {
   }
 
   render() {
+    const rgb = this.state.background.rgb;
+
     return (
       <div className="App">
-        <CloudinaryContext cloudName="<YOUR_CLOUD_NAME">
+        <CloudinaryContext cloudName="christekh">
+          <div id="demoContainer">
+            <div id="header">
+              <a href="http://cloudinary.com/">
+              <img width="172" height="38" src="http://res-1.cloudinary.com/cloudinary/image/asset/dpr_2.0/logo-e0df892053afd966cc0bfe047ba93ca4.png" alt="Cloudinary logo" />
+              </a>
+              <h1>Product Personalization Demo</h1>
+            </div>
+          </div>
           <div id="imageDemoContainer">
             <div id="mainImage">
               <ImageTransformations
@@ -54,6 +80,15 @@ class App extends Component {
                   );
                 })}
               </ul>
+            </div>
+          </div>
+          <div id="demoInputContainer">
+            <div className="inputSelections">
+              <h2>Shirt Color:</h2>
+              <SketchPicker
+                color={ this.state.background.hex }
+                onChangeComplete={ this.handleColorChange.bind(this) }
+              />
             </div>
           </div>
         </CloudinaryContext>
